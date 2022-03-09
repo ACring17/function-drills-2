efe639c875029b6b23c92d1eaee883b3432797bf
Andrew Cring <amcring1@gmail.com>
2022-03-08 20:17:32 -0700

    Solution for question 2 of callback and higher order method exercises

--- callbackAndHigherOrder.js
+++ callbackAndHigherOrder.js
@@ -36,15 +36,15 @@ var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan']
 */
 
 // CODE HERE 
-
+const first = (arr, cb) => cb(arr[0])
 
 // UNCOMMENT THE FUNCTION CALL BELOW
 // RUN THIS FILE WITH NODE
 // CHECK YOUR ANSWER
 
-// first(names, firstName => {
-//   console.log('The first name in names is ' + firstName)
-// })
+first(names, firstName => {
+    console.log('The first name in names is ' + firstName)
+})
 
 
 
